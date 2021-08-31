397cfdf03970dfd1000fd564dfa7c229053a2499
Quincy Wolfson <qwolfson@gmail.com>
2021-08-30 20:58:49 -0400

    studio

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -13,9 +13,36 @@ export class CrewComponent implements OnInit {
     {name: "Ellen Ochoa", firstMission: true}
   ];
 
+  memberBeingEdited: object = null;
+
+  
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(memberName: string, isFirst: boolean) {
+    for (let i = 0; i < this.crew.length; i++){
+      if (this.crew[i]["name"].toLowerCase() === memberName.toLowerCase()){
+        return
+      }
+    }
+    this.crew.push({name: memberName, firstMission: isFirst})
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
+
+ updateMember(updatedName: string, member: object) {
+  member['name'] = updatedName;
+  this.memberBeingEdited = null
+}
 
 }
